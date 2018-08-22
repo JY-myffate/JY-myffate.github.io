ea69f8ac9e8562a51737e3784fa5fbf8614617b6
JY-myffate <myffate@gmail.com>
2018-08-22 19:07:28 +0800

    Site updated: 2018-08-22 19:07:26

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/TEP.ico");
-         document.title = ' (>~<)! �������𣿣�';
+         document.title = '(>~<)!WARNING!!';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/favicon.ico");
-         document.title = ' ( O A O )~ û��Ŷ~' + OriginTitle;
+         document.title = ' ( O A O )~BOOOM!' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
